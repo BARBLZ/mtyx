adf54f009adc3e1c5c346e26b616bd5c058ac434
zzc <2512466133@qq.com>
2023-06-09 22:15:07 +0800

    统一异常处理类

---
+++ common/service-util/src/main/java/exception/MtyxException.java
@@ -0,0 +1,39 @@
+package exception;
+
+import lombok.Data;
+import result.ResultCodeEnum;
+
+// 自定义异常类
+@Data
+public class MtyxException extends RuntimeException{
+
+    //异常状态码
+    private Integer code;
+
+    /**
+     * 通过状态码和错误消息创建异常对象
+     * @param message
+     * @param code
+     */
+    public MtyxException(String message, Integer code) {
+        super(message);
+        this.code = code;
+    }
+
+    /**
+     * 接收枚举类型对象
+     * @param resultCodeEnum
+     */
+    public MtyxException(ResultCodeEnum resultCodeEnum) {
+        super(resultCodeEnum.getMessage());
+        this.code = resultCodeEnum.getCode();
+    }
+
+    @Override
+    public String toString() {
+        return "GuliException{" +
+                "code=" + code +
+                ", message=" + this.getMessage() +
+                '}';
+    }
+}
