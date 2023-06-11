4f4980ec8e00c9ab03f6ca91ac7bdedd2d643d08
zzc <2512466133@qq.com>
2023-06-11 11:10:44 +0800

    更改包的结构，都放在mtyx包下

---
+++ model/src/main/java/mtyx/vo/product/SkuInfoVo.java
@@ -0,0 +1,25 @@
+package mtyx.vo.product;
+
+import mtyx.model.product.SkuAttrValue;
+import mtyx.model.product.SkuImage;
+import mtyx.model.product.SkuInfo;
+import mtyx.model.product.SkuPoster;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class SkuInfoVo extends SkuInfo {
+
+	@ApiModelProperty(value = "海报列表")
+	private List<SkuPoster> skuPosterList;
+
+	@ApiModelProperty(value = "属性值")
+	private List<SkuAttrValue> skuAttrValueList;
+
+	@ApiModelProperty(value = "图片")
+	private List<SkuImage> skuImagesList;
+
+}
+
