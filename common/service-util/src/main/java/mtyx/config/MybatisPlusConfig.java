8a3fbc613d09d648af7083442220cefab13fa078
zzc <2512466133@qq.com>
2023-06-11 13:44:01 +0800

    对分页工具的修改

--- common/service-util/src/main/java/mtyx/config/MybatisPlusConfig.java
+++ common/service-util/src/main/java/mtyx/config/MybatisPlusConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@MapperScan("*.mapper")
+@MapperScan("mtyx.*.mapper")
 public class MybatisPlusConfig {
 
     /**
