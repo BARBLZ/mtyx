1354976562871f220edd71cd8403311a33d11ec2
zzc <2512466133@qq.com>
2023-06-12 15:51:00 +0800

    完成角色管理的增删改查

--- service/service-acl/src/main/java/mtyx/acl/controller/RoleController.java
+++ service/service-acl/src/main/java/mtyx/acl/controller/RoleController.java
@@ -11,6 +11,8 @@ import mtyx.vo.acl.RoleQueryVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @Api(tags = "用户管理接口")
 @RequestMapping("/admin/acl/role")
@@ -34,5 +36,55 @@ public class RoleController {
 
         return Result.ok(pageModle);
     }
+
+    /**
+     * 根据id获取角色
+     */
+    @ApiOperation("根据id获取角色")
+    @GetMapping("/get/{id}")
+    public Result getById(@PathVariable Long id) {
+        Role role = roleService.getById(id);
+        return Result.ok(role);
+    }
+
+    /**
+     * 新增角色
+     */
+    @ApiOperation("新增角色")
+    @PostMapping("save")
+    public Result save(@RequestBody Role role) {
+        boolean is_success = roleService.save(role);
+        return is_success ? Result.ok(null) : Result.fail(null);
+    }
+
+    /**
+     * 修改角色
+     */
+    @ApiOperation("修改角色")
+    @PutMapping("update")
+    public Result update(@RequestBody Role role) {
+        boolean is_success = roleService.updateById(role);
+        return is_success ? Result.ok(null) : Result.fail(null);
+    }
+    /**
+     * 删除角色
+     */
+    @ApiOperation("删除角色")
+    @DeleteMapping("/remove/{id}")
+    public Result remove(@PathVariable Long id) {
+        boolean is_success = roleService.removeById(id);
+        return is_success ? Result.ok(null) : Result.fail(null);
+
+    }
+     /**
+     * 批量删除角色
+     */
+     @ApiOperation("批量删除角色")
+     @DeleteMapping("batchRemove")
+     public Result batchRemove(@RequestBody List<Long> roleList) {
+         boolean is_success = roleService.removeByIds(roleList);
+         return is_success ? Result.ok(null) : Result.fail(null);
+
+     }
 
 }
